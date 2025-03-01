21b17b3c82ea79bac1206aca27949a4e1478aa34
vasanthgoud799 <vasanthgoud79@gmail.com>
2025-03-01 14:52:48 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 import React from "react";
 import { useState } from "react";
 
