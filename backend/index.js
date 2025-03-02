2b7fd869c38b184d030565391b757b379faab1d8
vasanthgoud799 <vasanthgoud79@gmail.com>
2025-03-02 13:04:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const databaseUrl = process.env.DATABASE_URL;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://task-note-phi.vercel.app","https://tasknote-hu4v.onrender.com"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://task-note-phi.vercel.app","https://tasknote-hu4v.onrender.com","https://task-note-axoswgrm9-vasanths-projects-af7e2121.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true, // Enable cookies
     allowedHeaders: ["Content-Type", "Authorization"],
